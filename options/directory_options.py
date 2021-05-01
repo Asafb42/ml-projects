034b90a4ee96404481876a28b3a06f6ce127f911
Asafb42 <asafb42@gmail.com>
2021-05-02 00:52:31 +0300

    Add a script to create data directory

---
+++ options/directory_options.py
@@ -0,0 +1,69 @@
+import argparse
+import os
+from util import util
+class DirectoryOptions():
+    """This class defines options used during dataset creation
+
+    """
+
+    def __init__(self):
+        """Reset the class; indicates the class hasn't been initailized"""
+        self.initialized = False
+
+    def initialize(self, parser):
+        # Define parameters
+        parser.add_argument('--source_dir_A', required=True, help='path to the first images directory (required)')
+        parser.add_argument('--source_dir_B', type=str, default='', help='path to the second images directory (optional)')
+        parser.add_argument('--name', type=str, default='dataset', help='dataset name')
+        parser.add_argument('--dest_dir', type=str, default='./{}'.format(parser.name), help='path to the destination directory')
+        parser.add_argument('--val', type=int, default=0, choices=range(0,101), help='precentage [0-100] of validation split of the data. Default is no validation split')
+        parser.add_argument('--test', type=int, default=0, choices=range(0,101), help='precentage [0-100] of test split of the data. Default is no test split')
+        self.initialized = True
+        return parser
+
+    def gather_options(self):
+        """Initialize our parser with options(only once).
+        """
+        if not self.initialized:  # check if it has been initialized
+            parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+            parser = self.initialize(parser)
+
+        # get the basic options
+        opt, _ = parser.parse_known_args()
+
+        # save and return the parser
+        self.parser = parser
+        return parser.parse_args()
+
+    def print_options(self, opt):
+        """Print and save options
+
+        It will print both current options and default values(if different).
+        It will save options into a text file / [dest_dir] / opt.txt
+        """
+        message = ''
+        message += '----------------- Options ---------------\n'
+        for k, v in sorted(vars(opt).items()):
+            comment = ''
+            default = self.parser.get_default(k)
+            if v != default:
+                comment = '\t[default: %s]' % str(default)
+            message += '{:>25}: {:<30}{}\n'.format(str(k), str(v), comment)
+        message += '----------------- End -------------------'
+        print(message)
+
+        # save to the disk
+        util.mkdirs(opt.dest_dir)
+        file_name = os.path.join(opt.dest_dir, 'dir_opt.txt')
+        with open(file_name, 'wt') as opt_file:
+            opt_file.write(message)
+            opt_file.write('\n')
+
+    def parse(self):
+        """Parse our options, create destination directory."""
+        opt = self.gather_options()
+
+        self.print_options(opt)
+
+        self.opt = opt
+        return self.opt
---
+++ scripts/create_data_dir.py
@@ -0,0 +1,37 @@
+"""
+Script to create data directories (trainA, trainB, testA, testB, etc.) in accordance to the expected dataroot of the main train/test scripts.
+The user can choose whether to create train, test and validation data directories from a given source or multiple source directories. 
+The user can also choose the train/val/test split ratio.
+"""
+from options.directory_options import DirectoryOptions
+from util import util
+import splitfolders
+import shutil
+import os
+
+if __name__ == '__main__':
+    opt = DirectoryOptions().parse() # get training options
+    
+    train_ratio = 100 - opt.val - opt.test # calculate train ratio
+    if train_ratio < 0:
+        raise ValueError("Invalid train ratio: {}.".format(train_ratio))
+
+    split_ratio = (train_ratio / 100, opt.val / 100, opt.test / 100)
+    
+    splitfolders.ratio(opt.source_dir_A, output=opt.dest_dir, ratio=split_ratio)
+    arrange_dir(opt.dest_dir, 'A')
+
+    splitfolders.ratio(opt.source_dir_B, output=opt.dest_dir, ratio=split_ratio)
+    arrange_dir(opt.dest_dir, 'B')
+
+
+def arrange_dir(dataroot, suffix):
+    
+    for (root, dirs, files) in os.walk('dataroot', topdown=true):
+            
+    # remove empty folders.
+    if not files:
+        os.rmdir(root)
+    # if the folder is not empty, add the suffix to the folder name.
+    else:
+        os.rename(root, os.pathroot + suffix)
