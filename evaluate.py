bce22066e4e7baffd453dc6b1e26945d82a685cc
Asafb42 <asafb42@gmail.com>
2021-06-16 19:54:37 +0300

    Revert "Seperate the print_options from parse function"
    This reverts commit 9c0f61c833591941e19e76abe82ab719d3a0673f.

--- evaluate.py
+++ evaluate.py
@@ -32,8 +32,6 @@ if __name__ == '__main__':
     model = create_model(opt)      # create a model given opt.model and other options
     model.setup(opt)               # regular setup: load and print networks; create schedulers
 
-    opt.print_options(opt)
-
     predictions = []
     labels = []
 
