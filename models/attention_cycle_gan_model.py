b5132cb5fca5370b1796528d44f09f3dbd829fc9
Asafb42 <asafb42@gmail.com>
2021-09-23 09:55:01 +0300

    Set attention model path parameter for train and test mode

--- models/attention_cycle_gan_model.py
+++ models/attention_cycle_gan_model.py
@@ -38,11 +38,11 @@ class AttentionCycleGANModel(BaseModel):
         Dropout is not used in the original CycleGAN paper.
         """
         parser.set_defaults(no_dropout=True)  # default CycleGAN did not use dropout
+        parser.add_argument('--attention_model_path', type=str, default=None, help='The path to the pretrained attention network to load')
         if is_train:
             parser.add_argument('--lambda_A', type=float, default=10.0, help='weight for cycle loss (A -> B -> A)')
             parser.add_argument('--lambda_B', type=float, default=10.0, help='weight for cycle loss (B -> A -> B)')
             parser.add_argument('--lambda_identity', type=float, default=0.5, help='use identity mapping. Setting lambda_identity other than 0 has an effect of scaling the weight of the identity mapping loss. For example, if the weight of the identity loss should be 10 times smaller than the weight of the reconstruction loss, please set lambda_identity = 0.1')
-            parser.add_argument('--attention_model_path', type=str, default=None, help='The path to the pretrained attention network to load')
 
         return parser
 
